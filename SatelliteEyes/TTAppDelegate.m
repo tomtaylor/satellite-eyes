8593f443940e6c2754677036e1b7da0d0fd47c30
Tom Taylor <tom@tomtaylor.co.uk>
2012-09-23 15:06:07 +0100

    4 space indentation in TTAppDelegate.m

--- SatelliteEyes/TTAppDelegate.m
+++ SatelliteEyes/TTAppDelegate.m
@@ -20,42 +20,42 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-  [DDLog addLogger:[DDASLLogger sharedInstance]];
-  [DDLog addLogger:[DDTTYLogger sharedInstance]];
-  
-  preferencesWindowController = [[TTPreferencesWindowController alloc] init];
-  
-  [self setUserDefaults];
-  
-  // 100MB disk cache for tile images
-  [[NSURLCache sharedURLCache] setDiskCapacity:100*1024^2];
-  
-  statusItemController = [[TTStatusItemController alloc] init];
-  mapManager = [[TTMapManager alloc] init];
-  
-  BOOL shouldCleanCache = [[NSUserDefaults standardUserDefaults] boolForKey:@"cleanCache"];
-  if (shouldCleanCache) {
-    [mapManager cleanCache];
-  }
-  
-  [[NSNotificationCenter defaultCenter] addObserverForName:TTMapManagerLocationPermissionDenied object:nil queue:nil usingBlock:^(NSNotification *note) {
-    [self shutdownWithLocationError];
-  }];
-  
-  [self doFirstRun];
-  [mapManager start];
-  
+    [DDLog addLogger:[DDASLLogger sharedInstance]];
+    [DDLog addLogger:[DDTTYLogger sharedInstance]];
+    
+    preferencesWindowController = [[TTPreferencesWindowController alloc] init];
+    
+    [self setUserDefaults];
+    
+    // 100MB disk cache for tile images
+    [[NSURLCache sharedURLCache] setDiskCapacity:100*1024^2];
+    
+    statusItemController = [[TTStatusItemController alloc] init];
+    mapManager = [[TTMapManager alloc] init];
+    
+    BOOL shouldCleanCache = [[NSUserDefaults standardUserDefaults] boolForKey:@"cleanCache"];
+    if (shouldCleanCache) {
+        [mapManager cleanCache];
+    }
+    
+    [[NSNotificationCenter defaultCenter] addObserverForName:TTMapManagerLocationPermissionDenied object:nil queue:nil usingBlock:^(NSNotification *note) {
+        [self shutdownWithLocationError];
+    }];
+    
+    [self doFirstRun];
+    [mapManager start];
+    
 }
 
 - (void)menuActionExit:(id)sender {
-  [[NSApplication sharedApplication] terminate:self];
+    [[NSApplication sharedApplication] terminate:self];
 }
 
 - (void)showPreferences:(id)sender {
-  [[NSApplication sharedApplication] activateIgnoringOtherApps:YES];
-  [preferencesWindowController showWindow:self];
-  [preferencesWindowController.window makeKeyAndOrderFront:self];
-  [preferencesWindowController.window makeFirstResponder:nil];
+    [[NSApplication sharedApplication] activateIgnoringOtherApps:YES];
+    [preferencesWindowController showWindow:self];
+    [preferencesWindowController.window makeKeyAndOrderFront:self];
+    [preferencesWindowController.window makeFirstResponder:nil];
 }
 
 - (void)forceMapUpdate:(id)sender {
@@ -63,57 +63,57 @@
 }
 
 - (void)checkForUpdates:(id)sender {
-  [[SUUpdater sharedUpdater] checkForUpdates:sender];
+    [[SUUpdater sharedUpdater] checkForUpdates:sender];
 }
 
 - (void)setUserDefaults {
-  [[NSUserDefaults standardUserDefaults] registerDefaults:
-   [NSDictionary dictionaryWithContentsOfFile:
-    [[NSBundle mainBundle] pathForResource:@"Defaults" ofType:@"plist"]]];
+    [[NSUserDefaults standardUserDefaults] registerDefaults:
+     [NSDictionary dictionaryWithContentsOfFile:
+      [[NSBundle mainBundle] pathForResource:@"Defaults" ofType:@"plist"]]];
 }
 
 - (void)doFirstRun {
-  NSString *firstRunKey = @"doneFirstRun";
-  BOOL doneFirstRun = [[NSUserDefaults standardUserDefaults] boolForKey:firstRunKey];
-  if (!doneFirstRun) {
-    [self doHelloAlert];
-    [self doStartupAlert];
-  }
-  [[NSUserDefaults standardUserDefaults] setBool:YES forKey:firstRunKey];
-  [[NSUserDefaults standardUserDefaults] synchronize];
+    NSString *firstRunKey = @"doneFirstRun";
+    BOOL doneFirstRun = [[NSUserDefaults standardUserDefaults] boolForKey:firstRunKey];
+    if (!doneFirstRun) {
+        [self doHelloAlert];
+        [self doStartupAlert];
+    }
+    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:firstRunKey];
+    [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
 - (void)doHelloAlert {
-  NSAlert *alert = [[NSAlert alloc] init];
-  [alert addButtonWithTitle:@"OK"];
-  [alert setMessageText:@"Welcome to Satellite Eyes"];
-  [alert setInformativeText:@"Satellite Eyes is now running in the status bar at the top right of your screen.\n\nIt will automatically change your desktop wallpaper to your current location.\n\nYou can adjust the preferences by clicking on the icon."];
-  [alert runModal];
+    NSAlert *alert = [[NSAlert alloc] init];
+    [alert addButtonWithTitle:@"OK"];
+    [alert setMessageText:@"Welcome to Satellite Eyes"];
+    [alert setInformativeText:@"Satellite Eyes is now running in the status bar at the top right of your screen.\n\nIt will automatically change your desktop wallpaper to your current location.\n\nYou can adjust the preferences by clicking on the icon."];
+    [alert runModal];
 }
 
 - (void)doStartupAlert {
-  NSAlert *startupAlert = [[NSAlert alloc] init];
-  [startupAlert addButtonWithTitle:@"Yes"];
-  [startupAlert addButtonWithTitle:@"No"];
-  [startupAlert setMessageText:@"Run Satellite Eyes at Startup?"];
-  [startupAlert setInformativeText:@"Satellite Eyes works best when it's run in the background all the time. Do you want it to run automatically at startup?"];
-  
-  NSInteger result = [startupAlert runModal];
-  if (result == NSAlertFirstButtonReturn) {
-    [LLManager setLaunchAtLogin:YES];
+    NSAlert *startupAlert = [[NSAlert alloc] init];
+    [startupAlert addButtonWithTitle:@"Yes"];
+    [startupAlert addButtonWithTitle:@"No"];
+    [startupAlert setMessageText:@"Run Satellite Eyes at Startup?"];
+    [startupAlert setInformativeText:@"Satellite Eyes works best when it's run in the background all the time. Do you want it to run automatically at startup?"];
     
-  } else if (result == NSAlertSecondButtonReturn) {
-    // do nothing
-  }
+    NSInteger result = [startupAlert runModal];
+    if (result == NSAlertFirstButtonReturn) {
+        [LLManager setLaunchAtLogin:YES];
+        
+    } else if (result == NSAlertSecondButtonReturn) {
+        // do nothing
+    }
 }
 
 - (void)shutdownWithLocationError {
-  NSAlert *alert = [[NSAlert alloc] init];
-  [alert addButtonWithTitle:@"OK"];
-  [alert setMessageText:@"Satellite Eyes Will Quit"];
-  [alert setInformativeText:@"Satellite Eyes needs permission to access your location, or it can't load the correct map.\n\nYou can enable Location Services from the Security & Privacy pane in System Preferences, and then restart the application."];
-  [alert runModal];
-  [NSApp terminate:nil];
+    NSAlert *alert = [[NSAlert alloc] init];
+    [alert addButtonWithTitle:@"OK"];
+    [alert setMessageText:@"Satellite Eyes Will Quit"];
+    [alert setInformativeText:@"Satellite Eyes needs permission to access your location, or it can't load the correct map.\n\nYou can enable Location Services from the Security & Privacy pane in System Preferences, and then restart the application."];
+    [alert runModal];
+    [NSApp terminate:nil];
 }
 
 @end
