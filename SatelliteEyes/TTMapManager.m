30f1be3acf9037afc6a876486ebe56f1297ac95c
Paul Schuberth <github@pschuberth.de>
2023-10-08 20:57:51 +0200

    chore: Change placeholder for Stadia token field

--- SatelliteEyes/TTMapManager.m
+++ SatelliteEyes/TTMapManager.m
@@ -131,6 +131,7 @@
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
 {
+    DDLogDebug(@"Change: %@", change);
     [self updateMap];
 }
 
